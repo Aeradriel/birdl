4623deb15fed31524d09ae954a603b8fed0519a6
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-01-19 18:28:01 -0500

    [A]Tests for Admin user deletiong

---
+++ spec/controllers/admin_user_deletion_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'Admin user deletion button' do
+  def sign_in(user)
+    user.confirm!
+    visit '/login'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'login_submit'
+  end
+
+  context 'with existing user', js: true do
+    before :each do
+      @admin = FactoryGirl.create(:admin)
+      FactoryGirl.create_list(:user, 5)
+
+      sign_in @admin
+      visit '/admin/users/'
+      all('#admin_userlist tr').each do |tr|
+        next if tr.has_css?('th')
+        tr.first('.admin_delete_button').click
+        break
+      end
+    end
+
+    it 'should display an alert to confirm the deletion' do
+      expect(page.driver.browser.switch_to.alert).to_not eq nil
+    end
+
+    it 'should not delete account when cancelling deletion' do
+      expect do
+        page.driver.browser.switch_to.alert.dismiss
+      end.to change(User, :count).by 0
+    end
+
+    it 'should delete account when confirming deletion' do
+      expect do
+        page.driver.browser.switch_to.alert.accept
+        visit '/'
+      end.to change(User, :count).by(-1)
+    end
+  end
+
+  context 'with existing user' do
+    it 'should display an error' do
+      @controller = Admin::CountriesController
+      @admin = FactoryGirl.create(:admin)
+
+      pending 'Undefined ??'
+      sign_in @admin
+      delete :delete, user_id: -1
+    end
+  end
+end

5e938295854864b41f4df63607db0b9589b30f67
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-02-20 05:05:48 -0500

    [C]Asset handling for production environnement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,18 +35,18 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  # config.assets.compile = true
+  # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  # config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to
   # config/initializers/assets.rb

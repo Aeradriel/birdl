dc66845665bb5fe161f6d8dbcd8bff88c53f77e6
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-02-21 13:29:33 -0500

    [C]Actions for events

--- app/controllers/events/face_to_face_controller.rb
+++ app/controllers/events/face_to_face_controller.rb
@@ -1,13 +1,35 @@
 module Events
   # Controller for GroupEvent
   class FaceToFaceController < ApplicationController
+    before_action :set_event, only: [:show, :edit, :update,
+                                     :destroy]
+
     def index
+      @events = Event.all
     end
 
     def show
     end
 
+    def new
+    end
+
     def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_event
+      @event = Event.find(params[:event_id])
     end
   end
 end

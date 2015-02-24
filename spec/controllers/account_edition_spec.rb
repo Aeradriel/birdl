945e5b28946b1d00867de9b569a9a8cca442473c
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-02-24 16:19:17 -0500

    [F]Fix du code dégueu d'Alexis

--- spec/controllers/account_edition_spec.rb
+++ spec/controllers/account_edition_spec.rb
@@ -18,35 +18,35 @@ describe 'The account edition page' do
   end
 
   it 'should raise an error when password is not provided' do
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(page).to have_css('#error_explanation')
   end
 
   it 'should indicate the wrong field' do
     fill_in 'user_email', with: 'bonjour@lol.com'
     fill_in 'user_current_password', with: 'iudjeiuzdhuize'
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(page).to have_css('.field_with_errors')
   end
 
   it 'should change unconfirmed address when filling the email form' do
     fill_in 'user_email', with: 'bonjour@lol.com'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).unconfirmed_email).to eq 'bonjour@lol.com'
   end
 
   it 'shouldn\'t change email address before it\'s confirmed' do
     fill_in 'user_email', with: 'bonjour@lol.com'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).email).to eq @user.email
   end
 
   it 'should change email address after confirmed the new address' do
     fill_in 'user_email', with: 'bonjour@lol.com'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     User.find(@user.id).confirm!
     expect(User.find(@user.id).email).to eq 'bonjour@lol.com'
   end
@@ -54,21 +54,21 @@ describe 'The account edition page' do
   it 'should change first name when editing in form' do
     fill_in 'user_first_name', with: 'Nouveau nom'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).first_name).to eq 'Nouveau nom'
   end
 
   it 'should change last name when editing in form' do
     fill_in 'user_last_name', with: 'Nouveau nom'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).last_name).to eq 'Nouveau nom'
   end
 
   it 'should change gender when editing in form' do
     select I18n.t(:female), from: 'user_gender'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).gender).to eq 0
   end
 
@@ -80,7 +80,7 @@ describe 'The account edition page' do
     visit '/users/edit'
     select 'England', from: 'user_country_id'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     user = User.find(@user.id).country
     expect(user).to eq Country.where(name: 'England').first
   end
@@ -89,7 +89,7 @@ describe 'The account edition page' do
     fill_in 'user_password', with: 'jemappelleguy'
     fill_in 'user_password_confirmation', with: 'jemappelleguy'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).valid_password?('jemappelleguy')).to eq true
   end
 
@@ -97,7 +97,7 @@ describe 'The account edition page' do
     fill_in 'user_password', with: 'jemappelleguy'
     fill_in 'user_password_confirmation', with: 'iojojojojoi'
     fill_in 'user_current_password', with: @user.password
-    click_button 'edit_submit'
+    click_button 'userEdit_submit'
     expect(User.find(@user.id).valid_password?('jemappelleguy')).to eq false
   end
 end

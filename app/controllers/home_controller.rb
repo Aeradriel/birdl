d9890cf7361a42bf2ef8cadaaac9bba5f7804302
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-02-25 14:05:19 -0500

    [F]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
   def modules_to_display
     scores = { achievements: 3, messages: 5, event: 4 }
     scores[:messages] = 10 if @current_user.received_messages.unread.count > 0
-    scores[:event] = 9 if @current_user.events.soon.count > 0
+    scores[:events] = 9 if @current_user.events.soon.count > 0
     scores[:achievements] = 6 if @current_user.achievements.recent.count > 0
     Hash[scores.sort_by { |_key, value| value }[0..2]]
   end

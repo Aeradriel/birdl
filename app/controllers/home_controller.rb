98a8ae063b6349e40a464ade31165a164d6c79eb
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-02-25 13:19:24 -0500

    [F]Modules for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
     @sent_messages = current_user.sent_messages[0..1]
     @received_messages = current_user.received_messages[0..1]
     @event = current_user.events.order(:date).first
-    @modules_to_display << :events if @event
   end
 
   private

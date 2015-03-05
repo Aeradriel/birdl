3c3ad4ebba26503e201066a319a68fe0d0e42ffc
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-03-04 20:18:15 -0500

    [A]Address display in group event

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,11 @@
 class Address < ActiveRecord::Base
   belongs_to :country
   belongs_to :user
+  has_many :events
+
+  def to_s
+    "#{num} #{street}\n" \
+    "#{zipcode} #{city}\n" \
+    "#{country.name}"
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,7 @@ class Event < ActiveRecord::Base
         }
 
   belongs_to :owner, class_name: 'User'
-  has_one :address
+  belongs_to :address
   has_many :participations
   has_many :users, through: :participations
 

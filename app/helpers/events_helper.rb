86cdf76bfd7142ee2620f1d3d5e137b3cf2bf43f
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-04-16 01:57:38 -0400

    [C]Disabling TourismTour for now

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,6 +3,6 @@ module EventsHelper
   def event_route(e)
     return "/events/facestofaces/#{e.id}" if e.is_a? FaceToFace
     return "/events/groupevents/#{e.id}" if e.is_a? GroupEvent
-    "/events/tourismtours/#{e.id}" if e.is_a? TourismTour
+    '/events/' if e.is_a? TourismTour
   end
 end
